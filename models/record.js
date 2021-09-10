272e9bea514f43a1a1b0846ca0d242abac934119
Bumble <nick1092387456@gmail.com>
2021-09-10 17:06:42 +0800

    feat: add mongoose

---
+++ models/record.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const recordSchema = new Schema({
+  name: { type: String, required: true },
+  merchant: { type: String, required: true },
+  category: { type: String, required: true },
+  date: { type: Date, required: true },
+  amount: { type: Number, require: true },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
+    required: true,
+  },
+})
+
+module.exports = mongoose.model('record', recordSchema)
