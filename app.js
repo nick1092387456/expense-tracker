792b1a6e315fca78e8c1b248b78b56b595e5b241
Bumble <nick1092387456@gmail.com>
2021-09-10 17:57:19 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose') // 載入 mongoose
 const exphbs = require('express-handlebars') //載入樣板引擎
 const Record = require('./models/record') //載入資料庫連結 Record
+const bodyParser = require('body-parser') //req.body存取器 (form資料存取器)
 
 const app = express()
 
@@ -27,13 +28,27 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true })) // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+
 // --路由設定--
-// 設定首頁路由
+// 首頁路由
 app.get('/', (req, res) => {
   Record.find() //找出全部紀錄
     .lean() //轉換格式成Javascript陣列
     .then((records) => res.render('index', { records })) //把records傳給index.hbs，並且渲染index
     .catch((errors) => console.log(errors)) //如果有錯誤console出來
+})
+
+// create路由
+app.get('/records/new', (req, res) => {
+  res.render('new')
+})
+
+app.post('/records', (req, res) => {
+  const name = req.body.name // 從 req.body 拿出new.hbs form裡的 name="name" 資料(value)
+  return Record.create({ name }) // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch((errors) => console.log(errors))
 })
 
 // 設定 port 3000
