0b52eb98645b1bfc31f9b7040db41c93199662df
Bumble <nick1092387456@gmail.com>
2021-09-11 19:02:15 +0800

    feat: add user modules & login.hbs

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router() // 呼叫 Express 路由器
 
 const home = require('./modules/home') //引入home.js modules
 const records = require('./modules/records')
+const users = require('./modules/users')
 
 // 準備引入路由模組
 router.use('/', home) // 將網址結構符合 / 字串的 request 導向 home modules
 router.use('/records', records) // 將網址結構符合 /records 字串開頭的 request 導向 records modules
+router.use('/users', users)
 
 // 匯出路由器
 module.exports = router
