7de39cb744b5048bf46021d7350664ea02dd046d
Bumble <nick1092387456@gmail.com>
2021-09-11 19:46:48 +0800

    feat: add user register feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,15 +1,37 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
 
 // 登入路由
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post('/login', (req, res) => {})
+router.post('login', async (req, res) => {})
 
 // 註冊路由
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', async (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  const theUser = await User.findOne({ email })
+  if (theUser) {
+    console.log('User already exists.')
+    res.render('register', {
+      name,
+      email,
+      password,
+      confirmPassword,
+    })
+  } else {
+    await User.create({
+      name,
+      email,
+      password,
+    })
+    res.redirect('/')
+  }
 })
 module.exports = router
