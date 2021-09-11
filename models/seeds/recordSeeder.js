2f4556bf1778e8dfc4e7b85be6b182c104f3c2f2
Bumble <nick1092387456@gmail.com>
2021-09-12 02:42:29 +0800

    modify recordSeeder

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,14 +1,49 @@
 const mongoose = require('mongoose')
 const db = require('../../config/mongoose')
-const RecordsJson = require('../datas/records.json')
+const records = require('../datas/records.json')
 const Record = require('../record') // 載入 record model
+const User = require('../user')
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '12345678',
+}
 
 //這裡db.on拿掉是因為在mongoose.js已經測試過了
 
 db.once('open', () => {
   console.log('record seeder start!')
-  Record.create(RecordsJson).then(() => {
-    db.close()
-    console.log('record seeder done!')
-  })
+  bcrypt
+    .genSalt(10)
+    .then((salt) => bcrypt.hash(SEED_USER.password, salt))
+    .then((hash) =>
+      User.create({
+        name: SEED_USER.name,
+        email: SEED_USER.email,
+        password: hash,
+      })
+    )
+    .then((user) => {
+      const userId = user._id
+      return Promise.all(
+        Array.from({ length: records.length }, (_, i) =>
+          Record.create({
+            name: records[i].name,
+            category: records[i].category,
+            date: records[i].date,
+            amount: records[i].amount,
+            userId,
+          })
+        )
+      )
+    })
+    .then(() => {
+      console.log('done.')
+      process.exit()
+    })
 })
