06c5c31a1ea2ab6ac9d7575bf19d55ec6929dad6
Bumble <nick1092387456@gmail.com>
2021-09-11 19:06:43 +0800

    feat: update user login page, users router. & add register.hbs

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
+// 登入路由
 router.get('/login', (req, res) => {
   res.render('login')
+})
+
+router.post('/login', (req, res) => {})
+
+// 註冊路由
+router.get('/register', (req, res) => {
+  res.render('register')
 })
 module.exports = router
